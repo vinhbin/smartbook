067dc5b8efd0567980ac9658f827b047069d3eb0
Shawnm-cyber <shawnmathew14@gmail.com>
2025-04-29 14:39:40 -0400

    Added additional shared_preferences package for storing user preferences along with listing variables needed for an optimal UI experience

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:smartbook/widgets/book_card.dart';
 import 'package:smartbook/services/google_books_api.dart';
 import 'package:smartbook/models/book.dart';
+import 'package:shared_preferences/shared_preferences.dart'; // For storing user preferences
 
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<Book>> _recommendedBooks;
+  final ScrollController _scrollController = ScrollController();
+  int _startIndex = 0;
+  final int _pageSize = 10;
+  bool _isLoading = false;
+  bool _hasMore = true;
+  List<String> _userGenres = []; // User's favorite genres
+  List<String> _recentRatings = []; // Store IDs of recently rated books
