a51611cbd333d4d6a0dfc2a3f3d74d1dc77c92ce
Shawnm-cyber <shawnmathew14@gmail.com>
2025-04-29 00:25:05 -0400

    Created and initialized Register Screen

---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
