f1d8c06f8906a5b2bfb3af829efd26939314d3d8
Shawnm-cyber <shawnmathew14@gmail.com>
2025-04-29 00:36:44 -0400

    Added formatting aspect for Register Screen and updated image in the assets folder

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -10,3 +10,21 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Register')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // Image at the top
+              Image.asset(
+                'assets/images/logo/register_screen.JPG', // Adjust path as needed
+                height: 100,
+              ),
+              SizedBox(height: 20),
