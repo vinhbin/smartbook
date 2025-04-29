10c74f163a41ccc2f580d355c4c563b816daea74
Shawnm-cyber <shawnmathew14@gmail.com>
2025-04-29 14:20:50 -0400

    Created and initialized Home Screen along with adjusting the image of Register Screen to be under the proper path of assets\images\logo folder

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:smartbook/widgets/book_card.dart';
+import 'package:smartbook/services/google_books_api.dart';
+import 'package:smartbook/models/book.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
