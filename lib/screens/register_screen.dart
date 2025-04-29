e53275fd1c39ca9e36c5484181bc15edb5d219d6
Shawnm-cyber <shawnmathew14@gmail.com>
2025-04-29 00:30:17 -0400

    Instance for FirebaseAuth, intended for field integrations for Register Screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -5,3 +5,8 @@ class RegisterScreen extends StatefulWidget {
   @override
   _RegisterScreenState createState() => _RegisterScreenState();
 }
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
