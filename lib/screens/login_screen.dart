5a583f15dbfa1f08823fd867fca21074cd9d8194
Shawnm-cyber <shawnmathew14@gmail.com>
2025-04-28 23:26:44 -0400

    Created and initialized Login Screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
