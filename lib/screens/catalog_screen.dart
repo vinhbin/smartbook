59e0fdce55e9cb0db7605aee3922a6e2613ec485
Shawnm-cyber <shawnmathew14@gmail.com>
2025-05-01 18:09:56 -0400

    Create variables to track needed values for result generation for books

--- lib/screens/catalog_screen.dart
+++ lib/screens/catalog_screen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-import 'services/google_books_service.dart';
+import 'package:smartbook/services/google_books_service.dart'; 
 import 'package:smartbook/models/book.dart';
 import 'package:smartbook/widgets/book_card.dart';
 
 class CatalogScreen extends StatefulWidget {
   @override
-  _CatalogScreen createState() => _CatalogScreen();
+  _CatalogScreenState createState() => _CatalogScreenState();
 }
+
+class _CatalogScreenState extends State<CatalogScreen> {
+  final _searchController = TextEditingController();
+  List<Book> _searchResults = [];
+  String _errorMessage = '';
+  bool _isLoading = false; // Added to track loading state
