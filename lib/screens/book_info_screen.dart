e6ce7c079e6cdb81471101ed8d8cda455f97be62
Shawnm-cyber <shawnmathew14@gmail.com>
2025-05-02 16:47:13 -0400

    Creating certain criteria or fields that need to be there when the user is trying to research said book along with including Home Screen icon at top to return to if needed

--- lib/screens/book_info_screen.dart
+++ lib/screens/book_info_screen.dart
@@ -1,6 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:smartbook/models/book.dart';
 import 'package:smartbook/services/google_books_service.dart';
 import 'package:smartbook/widgets/rating_stars.dart'; // We will have a RatingStars widget
 
 class BookInfoScreen extends StatelessWidget {
   const BookInfoScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Book book = ModalRoute.of(context)!.settings.arguments as Book;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(book.title),
+        leading: IconButton(
+          icon: const Icon(Icons.home),
+          onPressed: () {
+            _navigateToHome(context);
+          },
+        ),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          _buildBookCover(book.thumbnail),
+          const SizedBox(height: 16),
+          _buildBookTitleAndAuthor(book.title, book.authors),
+          const SizedBox(height: 8),
+          RatingStars(rating: book.rating),
+          const SizedBox(height: 16),
+          _buildDescription(book.description),
+          const SizedBox(height: 24),
+          _buildActionButtons(context, book),
+        ],
+      ),
